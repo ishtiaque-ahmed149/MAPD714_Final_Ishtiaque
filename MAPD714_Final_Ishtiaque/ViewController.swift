4084ebbf12e00c94fe7956f32074ade4aaae0d95
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2021-12-16 09:39:41 -0500

    now going to second screen

--- MAPD714_Final_Ishtiaque/ViewController.swift
+++ MAPD714_Final_Ishtiaque/ViewController.swift
@@ -1,9 +1,10 @@
-//
 //  ViewController.swift
 //  MAPD714_Final_Ishtiaque
 //
 //  Created by Ishtiaque Ahmed on 12/16/21.
-//
+//  Final Exam, MAPD714 (iOS development)
+//  Changes Made: First UI attemp (Dec 15th), Changed looks of the UI from the first attempt, added launchscreen (Dec 16th), completed first screen with getting proper results switching mertic/emperial (Dec 16th),
+//  Description: This is a BMI Calculator with 2 screens and database capabilities.
 
 import UIKit
 
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var weightt: UITextField!
     @IBOutlet weak var heightt: UITextField!
     
+    @IBOutlet weak var switch1: UISwitch!
     @IBOutlet weak var BMIshow: UILabel!
     @IBOutlet weak var helloname: UILabel!
     
@@ -28,9 +30,18 @@ class ViewController: UIViewController {
     {
         let weightt = Float(self.weightt.text!)!
         let heightt = Float(self.heightt.text!)!
-        let bmi = Float(weightt/(heightt*heightt))
-        self.helloname.text = "Hello, " + self.namee.text!
-        self.BMIshow.text = String(format:"%2f", bmi)
+        if switch1.isOn
+        {
+            let bmi = Float(weightt/(heightt*heightt))
+            self.helloname.text = "Hello, " + self.namee.text!
+            self.BMIshow.text = String(format:"%2f", bmi)
+        }
+        else
+        {
+            let bmi = Float((weightt*703)/(heightt*heightt))
+            self.helloname.text = "Hello, " + self.namee.text!
+            self.BMIshow.text = String(format:"%2f", bmi)
+        }
     }
     
 }
